c39be3c9f46d0c41fe82637908b612eb6b9e27fe
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2023-01-12 18:16:33 -0500

    update

---
+++ React/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="Botoncito">Enviar</button>
+    
+    </div>
+  );
+}
+
+export default App;
